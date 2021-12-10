ec2141646a5e38f36483ccd600148252dd8fa9e8
Kadircan162 <kadircangulde@gmail.com>
2021-12-10 13:36:39 +0300

    Rep

--- src/ReplitAssignments2021/LettersCounter.java
+++ src/ReplitAssignments2021/LettersCounter.java
@@ -9,17 +9,18 @@ public class LettersCounter {
         String output = "";
         int index;
         int counter;
+
         while (!str.isEmpty()){ //for each iteration, searched char is replaced with ""
           index = 0;
           counter = 0;
           char c = str.charAt(index);
-            for (int j=0; j<str.length(); j++){
-                if(c == str.charAt(j)){
+            for (int i=0; i<str.length(); i++){
+                if(c == str.charAt(i)){
                 counter++;
                 }
             }
           output += (counter) + "" + c;
-          str = str.replace(""+c, "");
+          str = str.replace(""+c, ""); //bbcccc cccc ""
         }
         return output;
     }
