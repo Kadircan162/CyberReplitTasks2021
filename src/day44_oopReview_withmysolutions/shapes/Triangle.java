d6a71112171204ab3204e15bc106c317f5cd6dbd
CydCoderKadircan <kadircangulde@gmail.com>
2022-01-04 09:46:29 +0300

    oop review triangle added

---
+++ src/day44_oopReview_withmysolutions/shapes/Triangle.java
@@ -0,0 +1,13 @@
+package day44_oopReview_withmysolutions.shapes;
+
+public class Triangle extends Shape{
+
+    public Triangle() {
+        type = "triangle";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing a triangle");
+    }
+}
